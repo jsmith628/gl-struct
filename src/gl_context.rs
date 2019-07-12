86ab4530209f099d3299ec2156c27c02e22abc5d
Joshua Smith <anvil775@gmail.com>
2019-07-12 17:25:28 -0500

    made GLContext take a GL version as a generic parameter

--- src/gl_context.rs
+++ src/gl_context.rs
@@ -1,28 +1,28 @@
 use super::*;
-
+use gl_version::GL;
 
 ///
 ///A struct for keeping track of global GL state while
 ///enforcing rust-like borrow rules on things like gl settings
 ///and bind points
 ///
-pub struct GLContext {
-
+pub struct GLContext<V:GL> {
+    pub version: V,
 }
 
-impl GLContext {
-    pub unsafe fn load_with<F:Fn(&str) -> *const GLvoid>(api_addr: F) -> (GL10,Self) {
+impl GLContext<GL10> {
+    pub unsafe fn load_with<F:Fn(&str) -> *const GLvoid>(api_addr: F) -> Self {
         gl::load_with(api_addr);
-        (GL10::assume_loaded(), GLContext {})
+        GLContext {version: GL10::assume_loaded()}
     }
 }
 
-impl !Send for GLContext {}
-impl !Sync for GLContext {}
+impl<V:GL> !Send for GLContext<V> {}
+impl<V:GL> !Sync for GLContext<V> {}
 
 pub trait ContextProvider {
     type ContextError: Debug;
-    fn make_current(self) -> Result<(GL10,GLContext),Self::ContextError>;
+    fn make_current(self) -> Result<GLContext<GL10>,Self::ContextError>;
 }
 
 #[cfg(feature = "glfw_context")]
@@ -32,7 +32,7 @@ mod glfw_impl {
 
     impl<'a> ContextProvider for &'a mut glfw::Window {
         type ContextError = !;
-        fn make_current(self) -> Result<(GL10,GLContext),!> {
+        fn make_current(self) -> Result<GLContext<GL10>,!> {
             unsafe {
                 glfw::Context::make_current(self);
                 Some(GLContext::load_with(|name| self.get_proc_address(name)))
@@ -49,7 +49,7 @@ mod glutin_impl {
 
     impl<T:glutin::ContextCurrentState> ContextProvider for glutin::Context<T> {
         type ContextError = (Self, glutin::ContextError);
-        fn make_current(self) -> Result<(GL10,GLContext),Self::ContextError> {
+        fn make_current(self) -> Result<GLContext<GL10>,Self::ContextError> {
             unsafe {
                 glutin::Context::<T>::make_current(self).map(
                     |context| GLContext::load_with(|name| context.get_proc_address(name) as *const GLvoid)
