8b7de9d6530505c5ef89528d226f2ba1d3524ad4
Joshua Smith <anvil775@gmail.com>
2019-11-30 15:20:59 -0600

    added methods in Image and ImageMut for getting the level 0 dimensions

--- src/object/texture/image.rs
+++ src/object/texture/image.rs
@@ -87,23 +87,21 @@ impl<'a,F,T:TextureTarget<F>> Image<'a,F,T> {
     pub fn level(&self) -> GLuint { self.level }
 
     #[inline]
-    unsafe fn get_parameter_iv(&self, pname:GLenum) -> GLint {
+    unsafe fn get_level_parameter_iv(&self, lvl:GLint, pname:GLenum) -> GLint {
         let mut param = MaybeUninit::uninit();
-        if gl::GetTextureLevelParameteriv::is_loaded() {
-            gl::GetTextureLevelParameteriv(self.id(), self.level() as GLint, pname, param.as_mut_ptr());
+        if T::glenum()!=gl::TEXTURE_CUBE_MAP && gl::GetTextureLevelParameteriv::is_loaded() {
+            gl::GetTextureLevelParameteriv(self.id(), lvl, pname, param.as_mut_ptr());
         } else {
             T::bind_loc_level().map_bind(self,
-                |b| {
-                    let target = if b.target_id()==gl::TEXTURE_CUBE_MAP {
-                        gl::TEXTURE_CUBE_MAP_POSITIVE_X
-                    } else {
-                        b.target_id()
-                    };
-                    gl::GetTexLevelParameteriv(target, self.level() as GLint, pname, param.as_mut_ptr())
-                }
+                |_| gl::GetTexLevelParameteriv(self.face.into(), lvl, pname, param.as_mut_ptr())
             );
         }
         param.assume_init()
+    }
+
+    #[inline]
+    unsafe fn get_parameter_iv(&self, pname:GLenum) -> GLint {
+        self.get_level_parameter_iv(self.level() as GLint, pname)
     }
 
     pub fn width(&self) -> usize { unsafe {self.get_parameter_iv(gl::TEXTURE_WIDTH) as usize} }
@@ -116,6 +114,19 @@ impl<'a,F,T:TextureTarget<F>> Image<'a,F,T> {
             if coords>0 {self.width()} else {1},
             if coords>1 {self.height()} else {1},
             if coords>2 {self.depth()} else {1},
+        )
+    }
+
+    pub fn base_width(&self) -> usize { unsafe {self.get_level_parameter_iv(0, gl::TEXTURE_WIDTH) as usize} }
+    pub fn base_height(&self) -> usize { unsafe {self.get_level_parameter_iv(0, gl::TEXTURE_HEIGHT) as usize} }
+    pub fn base_depth(&self) -> usize { unsafe {self.get_level_parameter_iv(0, gl::TEXTURE_DEPTH) as usize} }
+
+    pub fn base_dim(&self) -> T::Dim {
+        let coords = T::Dim::dim();
+        T::Dim::new(
+            if coords>0 {self.base_width()} else {1},
+            if coords>1 {self.base_height()} else {1},
+            if coords>2 {self.base_depth()} else {1},
         )
     }
 
@@ -161,6 +172,12 @@ impl<'a,F,T:TextureTarget<F>> ImageMut<'a,F,T> {
     pub fn depth(&self) -> usize { self.as_immut().depth() }
 
     pub fn dim(&self) -> T::Dim { self.as_immut().dim() }
+
+    pub fn base_width(&self) -> usize { self.as_immut().base_width() }
+    pub fn base_height(&self) -> usize { self.as_immut().base_height() }
+    pub fn base_depth(&self) -> usize { self.as_immut().base_depth() }
+
+    pub fn base_dim(&self) -> T::Dim { self.as_immut().base_dim() }
 
 }
 
