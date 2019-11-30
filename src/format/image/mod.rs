74eb617be06748049045209b42d334ca1328f2e0
Joshua Smith <anvil775@gmail.com>
2019-11-30 03:50:17 -0600

    added Image impls for the slice types

--- src/format/image/mod.rs
+++ src/format/image/mod.rs
@@ -17,12 +17,12 @@ pub trait ImageSrc<F:ClientFormat>: PixelSrc<F> {
     fn lsb_first(&self) -> bool;
     fn row_alignment(&self) -> PixelRowAlignment;
 
-    fn skip_pixels(&self) -> usize;
-    fn skip_rows(&self) -> usize;
-    fn skip_images(&self) -> usize;
+    fn skip_pixels(&self) -> usize {0}
+    fn skip_rows(&self) -> usize {0}
+    fn skip_images(&self) -> usize {0}
 
-    fn row_length(&self) -> usize;
-    fn image_height(&self) -> usize;
+    fn row_length(&self) -> usize {0}
+    fn image_height(&self) -> usize {0}
 
     fn width(&self) -> NonZeroUsize;
     fn height(&self) -> NonZeroUsize;
@@ -49,3 +49,58 @@ pub trait ImageDst<F:ClientFormat> = ImageSrc<F> + PixelDst<F>;
 pub trait OwnedImage<F:ClientFormat>: ImageSrc<F> {
     unsafe fn from_gl<GL:FnOnce(PixelStoreSettings, PixelPtrMut<F>)>(dim: [NonZeroUsize;3], gl:GL) -> Self;
 }
+
+macro_rules! impl_img_src_slice {
+    (for<$($a:lifetime,)* $P:ident> $ty:ty) => {
+        impl<$($a,)* F:ClientFormat, $P:Pixel<F>> ImageSrc<F> for $ty {
+
+            fn swap_bytes(&self) -> bool {$P::swap_bytes()}
+            fn lsb_first(&self) -> bool {$P::lsb_first()}
+            fn row_alignment(&self) -> PixelRowAlignment {PixelRowAlignment(1)}
+
+            fn width(&self) -> NonZeroUsize {NonZeroUsize::new(self.len()).expect("Images must not be zero-sized")}
+            fn height(&self) -> NonZeroUsize {NonZeroUsize::new(1).unwrap()}
+            fn depth(&self) -> NonZeroUsize {NonZeroUsize::new(1).unwrap()}
+
+        }
+    }
+}
+
+macro_rules! impl_own_img_slice {
+    (for<$($a:lifetime,)* $P:ident> $ty:ty) => {
+        impl<$($a,)* F:ClientFormat, $P:Pixel<F>> OwnedImage<F> for $ty {
+            unsafe fn from_gl<GL:FnOnce(PixelStoreSettings, PixelPtrMut<F>)>(
+                dim: [NonZeroUsize;3], gl:GL
+            ) -> Self {
+                let count = dim[0].get()
+                    .checked_mul(dim[1].get())
+                    .and_then(|m| m.checked_mul(dim[2].get()))
+                    .expect("Overflow when computing buffer size");
+
+                let settings = PixelStoreSettings {
+                    swap_bytes: $P::swap_bytes(),
+                    lsb_first: $P::lsb_first(),
+                    row_alignment: PixelRowAlignment(1),
+                    skip_pixels: 0, skip_rows: 0, skip_images: 0,
+                    row_length: 0, image_height: 0,
+                };
+
+                Self::from_pixels(count, |ptr| gl(settings, ptr))
+            }
+        }
+    }
+}
+
+impl_img_src_slice!(for<P> [P]);
+
+impl_img_src_slice!(for<P> Box<[P]>);
+impl_own_img_slice!(for<P> Box<[P]>);
+
+impl_img_src_slice!(for<P> Vec<P>);
+impl_own_img_slice!(for<P> Vec<P>);
+
+impl_img_src_slice!(for<P> ::std::rc::Rc<[P]>);
+impl_own_img_slice!(for<P> ::std::rc::Rc<[P]>);
+
+impl_img_src_slice!(for<'a,P> ::std::borrow::Cow<'a,[P]>);
+impl_own_img_slice!(for<'a,P> ::std::borrow::Cow<'a,[P]>);
