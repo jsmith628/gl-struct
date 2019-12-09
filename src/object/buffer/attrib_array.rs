c321a2cd8b0771369aa31e9c47b07a8a39c87c81
Joshua Smith <anvil775@gmail.com>
2019-12-08 19:07:27 -0600

    added SplitAttribs impl for AttribData types

--- src/object/buffer/attrib_array.rs
+++ src/object/buffer/attrib_array.rs
@@ -32,10 +32,18 @@ pub unsafe trait SplitAttribs<'a, A:Copy>: Copy {
     fn split<B:Initialized>(buf:Slice<'a,[Self],B>) -> A;
 }
 
+unsafe impl<'a, A:GLSLType, T:AttribData<A::AttribFormat>> SplitAttribs<'a,AttribArray<'a,A>> for T {
+    fn split<B:Initialized>(buf:Slice<'a,[T],B>) -> AttribArray<'a,A> {
+        unsafe {
+            AttribArray::from_raw_parts(T::format(), buf.id(), size_of::<T>(), buf.offset())
+        }
+    }
+}
+
 macro_rules! impl_split_tuple {
 
     () => {
-        impl_split_tuple!(@loop {} T1:A1 T2:A2 T3:A3 T4:A4 T5:A5 T6:A6 T7:A7 T8:A8 T9:A9 T10:A10 T11:A11 T12:A12);
+        impl_split_tuple!(@loop {T1:A1} T2:A2 T3:A3 T4:A4 T5:A5 T6:A6 T7:A7 T8:A8 T9:A9 T10:A10 T11:A11 T12:A12);
     };
 
     (@loop {$($T:ident:$A:ident)*} ) => { impl_split_tuple!(@impl $($T:$A)*); };
@@ -49,8 +57,8 @@ macro_rules! impl_split_tuple {
 
             #[allow(unused_variables, unused_mut, unused_assignments)]
             fn split<B:Initialized>(buf:Slice<'a,[Self],B>) -> ($(AttribArray<'a,$A>,)*) {
-                let (id, offset, stride) = (buf.id(), buf.offset(), size_of::<Self>());
-                let mut pointer = 0;
+                let (id, stride) = (buf.id(), size_of::<Self>());
+                let mut pointer = buf.offset();
                 (
                     $(
                         unsafe {
