add9814c38237f1f4556a3bcbbb09b813d8b7dd4
Joshua Smith <anvil775@gmail.com>
2019-12-08 19:02:50 -0600

    added SplitAttribs impl for 12-tuples

--- src/object/buffer/attrib_array.rs
+++ src/object/buffer/attrib_array.rs
@@ -38,7 +38,7 @@ macro_rules! impl_split_tuple {
         impl_split_tuple!(@loop {} T1:A1 T2:A2 T3:A3 T4:A4 T5:A5 T6:A6 T7:A7 T8:A8 T9:A9 T10:A10 T11:A11 T12:A12);
     };
 
-    (@loop {$($T:ident:$A:ident)*} ) => {};
+    (@loop {$($T:ident:$A:ident)*} ) => { impl_split_tuple!(@impl $($T:$A)*); };
     (@loop {$($T:ident:$A:ident)*} $T0:ident:$A0:ident $($rest:tt)*) => {
         impl_split_tuple!(@impl $($T:$A)*);
         impl_split_tuple!(@loop {$($T:$A)* $T0:$A0}  $($rest)*);
