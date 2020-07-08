3b78a9c365a0f56e802f325f51cc2857b8782df2
Joshua Smith <anvil775@gmail.com>
2020-07-08 01:24:02 -0500

    added from_slice method to AttribArray

--- src/object/vertex_array/attrib_array.rs
+++ src/object/vertex_array/attrib_array.rs
@@ -20,14 +20,19 @@ impl<'a,A:GLSLType> AttribArray<'a,A> {
     #[inline] pub fn offset(&self) -> usize { self.pointer }
     #[inline] pub fn pointer(&self) -> *const GLvoid { self.pointer as *const _ }
 
-    #[inline]
     pub unsafe fn from_raw_parts(fmt:A::AttribFormat, id:GLuint, stride:usize, ptr:usize) -> Self {
         AttribArray { id:id, format:fmt, stride:stride, pointer:ptr, buf:PhantomData }
     }
 
+    pub fn from_slice<T, B:Initialized>(buf: Slice<'a,[T],B>) -> Self
+    where T: AttribData<GLSL=A, Format=A::AttribFormat>
+    {
+        buf.into()
+    }
+
 }
 
-impl<'b, A:GLSLType, B:Initialized, T> From<Slice<'b,[T],B>> for AttribArray<'b,A>
+impl<'b, A:GLSLType, T, B:Initialized> From<Slice<'b,[T],B>> for AttribArray<'b,A>
 where T: AttribData<GLSL=A, Format=A::AttribFormat>
 {
     fn from(buf: Slice<'b,[T],B>) -> Self {
