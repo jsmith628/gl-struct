3738c152498574419c30c31151c31857c60e0835
Joshua Smith <anvil775@gmail.com>
2020-08-21 18:48:52 -0500

    moved Pixel to crate::format

--- src/format/pixel.rs
+++ src/format/pixel.rs
@@ -393,3 +393,122 @@ unsafe impl PixelLayout for DepthStencilLayout {
 }
 
 display_from_debug!(DepthStencilLayout);
+
+
+pub unsafe trait Pixel<F: PixelLayout>: Copy+PartialEq {
+    fn format() -> F;
+    fn swap_bytes() -> bool {false}
+    fn lsb_first() -> bool {false}
+}
+
+macro_rules! impl_int {
+    ($($prim:ident $ty:ident)*) => {
+        $(
+            impl_int!(@impl $prim; RED RED_INTEGER $ty);
+            impl_int!(@impl [$prim;1]; RED RED_INTEGER $ty);
+            impl_int!(@impl [$prim;2]; RG RG_INTEGER $ty);
+            impl_int!(@impl [$prim;3]; RGB RGB_INTEGER $ty);
+            impl_int!(@impl [$prim;4]; RGBA RGBA_INTEGER $ty);
+
+            unsafe impl Pixel<DepthLayout> for $prim {
+                fn format() -> DepthLayout { IntType::$ty.into() }
+            }
+            unsafe impl Pixel<DepthLayout> for [$prim;1] {
+                fn format() -> DepthLayout { IntType::$ty.into() }
+            }
+            unsafe impl Pixel<StencilLayout> for $prim {
+                fn format() -> StencilLayout { IntType::$ty.into() }
+            }
+            unsafe impl Pixel<StencilLayout> for [$prim;1] {
+                fn format() -> StencilLayout { IntType::$ty.into() }
+            }
+
+        )*
+    };
+
+    (@impl $prim:ty; $fmt1:ident $fmt2:ident $ty:ident) => {
+        unsafe impl Pixel<IntLayout> for $prim {
+            fn format() -> IntLayout { IntLayout::Integer(IntColorComponents::$fmt2, IntType::$ty) }
+        }
+        unsafe impl Pixel<FloatLayout> for $prim {
+            fn format() -> FloatLayout { FloatLayout::Normalized(ColorComponents::$fmt1, IntType::$ty) }
+        }
+    };
+
+}
+
+impl_int!{
+    GLbyte Byte
+    GLubyte UByte
+    GLshort Short
+    GLushort UShort
+    GLint Int
+    GLuint UInt
+}
+
+macro_rules! impl_float {
+    ($($prim:ident $ty:ident)*) => {
+        $(
+            impl_float!(@impl $prim; RED $ty);
+            impl_float!(@impl [$prim;1]; RED $ty);
+            impl_float!(@impl [$prim;2]; RG $ty);
+            impl_float!(@impl [$prim;3]; RGB $ty);
+            impl_float!(@impl [$prim;4]; RGBA $ty);
+
+            unsafe impl Pixel<DepthLayout> for $prim {
+                fn format() -> DepthLayout { FloatType::$ty.into() }
+            }
+            unsafe impl Pixel<DepthLayout> for [$prim;1] {
+                fn format() -> DepthLayout { FloatType::$ty.into() }
+            }
+
+            unsafe impl Pixel<DepthStencilLayout> for $prim {
+                fn format() -> DepthStencilLayout { FloatType::$ty.into() }
+            }
+            unsafe impl Pixel<DepthStencilLayout> for [$prim;1] {
+                fn format() -> DepthStencilLayout { FloatType::$ty.into() }
+            }
+
+        )*
+    };
+
+    (@impl $prim:ty; $fmt:ident $ty:ident) => {
+        unsafe impl Pixel<FloatLayout> for $prim {
+            fn format() -> FloatLayout { FloatLayout::Float(ColorComponents::$fmt, FloatType::$ty) }
+        }
+    };
+
+}
+
+impl_float!(GLfloat Float);
+
+macro_rules! impl_ivec {
+    ($($vec:ident $inner:ty),*) => {
+        $(
+            unsafe impl Pixel<IntLayout> for $vec {
+                fn format() -> IntLayout { <$inner as Pixel<IntLayout>>::format() }
+            }
+        )*
+    }
+}
+
+macro_rules! impl_vec {
+    ($($vec:ident $inner:ty),*) => {
+        $(
+            unsafe impl Pixel<FloatLayout> for $vec {
+                fn format() -> FloatLayout { <$inner as Pixel<FloatLayout>>::format() }
+            }
+        )*
+    }
+}
+
+use glsl::*;
+
+//TODO: add the 3D vecs after you revert the alignment requirements
+
+impl_ivec!(ivec2 [GLint;2], ivec4 [GLint;4], uvec2 [GLuint;2], uvec4 [GLuint;4]);
+impl_vec!{
+    ivec2 [GLint;  2], ivec4 [GLint;  4],
+    uvec2 [GLuint; 2], uvec4 [GLuint; 4],
+     vec2 [GLfloat;2],  vec4 [GLfloat;4]
+}
